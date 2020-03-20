a9bbd23d7c2e1f9e8c92637a764bd8a1ce7e83a9
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-03-20 20:50:44 +0100

    Wczytanie macierzy

--- klient.py
+++ klient.py
@@ -3,10 +3,32 @@
 
 from sys import exit
 from multiprocessing.managers import BaseManager
+from typing import List
 
+# TODO: Load as parameters
+# Server connection data
 SERVER_ADRES = '127.0.0.1'
 SERVER_PORT = 2332
 SERVER_KEY = b'key'
+
+# File data
+MATRIX_FILE_NAME = 'A.dat'
+VECTOR_FILE_NAME = 'X.dat'
+
+
+def loadMatrix(file_name: str) -> List[List[float]]:
+    """
+    Import matrix from file.
+    Matrix is stored in row oriented list.
+    Such that each row is a nested list.
+    """
+
+    with open(file_name, 'r') as file:
+        row_count = int(file.readline())
+        column_count = int(file.readline())
+
+        return [[float(file.readline()) for _ in range(column_count)]
+                for _ in range(row_count)]
 
 
 class CalculationManager(BaseManager):
@@ -15,6 +37,8 @@ class CalculationManager(BaseManager):
 
 CalculationManager.register('get_tasks_queue')
 CalculationManager.register('get_results_queue')
+
+CalculationManager.register('get_wektor')
 
 manager = CalculationManager(
     address=(SERVER_ADRES, SERVER_PORT), authkey=SERVER_KEY)
@@ -26,5 +50,10 @@ except ConnectionRefusedError:
     exit(-1)
 
 tasks_queue = manager.get_tasks_queue()
+wektor = manager.get_wektor()
 
 # TODO: Load data, create tasks and enqueue them
+matrix = loadMatrix(MATRIX_FILE_NAME)
+print(len(matrix))
+print(len(matrix[0]))
+# print(matrix)
