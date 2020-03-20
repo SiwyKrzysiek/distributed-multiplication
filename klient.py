345d6154a2f0a25e0922ffab8ad87eeab5327b93
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-03-20 21:28:40 +0100

    Zapisanie wekotra we wspólnej pamięci

--- klient.py
+++ klient.py
@@ -3,7 +3,8 @@
 
 from sys import exit
 from multiprocessing.managers import BaseManager
-from typing import List
+from typing import List, Tuple
+import math
 
 # TODO: Load as parameters
 # Server connection data
@@ -14,6 +15,8 @@ SERVER_KEY = b'key'
 # File data
 MATRIX_FILE_NAME = 'A.dat'
 VECTOR_FILE_NAME = 'X.dat'
+
+TASK_COUNT = 5
 
 
 def loadMatrix(file_name: str) -> List[List[float]]:
@@ -31,6 +34,27 @@ def loadMatrix(file_name: str) -> List[List[float]]:
                 for _ in range(row_count)]
 
 
+def split_to_ranges(vector_lenght: int, range_count: int) -> List[Tuple[int, int]]:
+    """
+    Divide single range into rangeCount parts. Each part takes form of [a, b)
+    Example: splitIntoRanges(10, 3) => [0, 4), [4, 8), [8, 10)
+    """
+    part_size = math.ceil(vector_lenght / range_count)
+
+    ranges = []
+    index = 0
+    for _ in range(range_count):
+        if index < vector_lenght:
+            r = index, min(index + part_size, vector_lenght)
+
+            ranges.append(r)
+            index = r[1]
+        else:  # There are more ranges than vector length
+            ranges.append((0, 0))
+
+    return ranges
+
+
 class CalculationManager(BaseManager):
     """Shared manager class"""
 
@@ -38,7 +62,7 @@ class CalculationManager(BaseManager):
 CalculationManager.register('get_tasks_queue')
 CalculationManager.register('get_results_queue')
 
-CalculationManager.register('get_wektor')
+CalculationManager.register('get_vector')
 
 manager = CalculationManager(
     address=(SERVER_ADRES, SERVER_PORT), authkey=SERVER_KEY)
@@ -50,10 +74,21 @@ except ConnectionRefusedError:
     exit(-1)
 
 tasks_queue = manager.get_tasks_queue()
-wektor = manager.get_wektor()
+vektor = manager.get_vector()
 
 # TODO: Load data, create tasks and enqueue them
 matrix = loadMatrix(MATRIX_FILE_NAME)
-print(len(matrix))
-print(len(matrix[0]))
+
+# Save vector to shared memory
+vektor.clear()
+vektor.extend(loadMatrix(VECTOR_FILE_NAME))
+
+# print(len(matrix))
+# print(len(matrix[0]))
 # print(matrix)
+
+# print(len(manager.get_vector()))
+print(vektor.copy()[:10])
+
+# ---------- Create tasks ---------- #
+# Matrix will be divided into groups of rows
--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 from multiprocessing import Queue
 from multiprocessing.managers import BaseManager
 
+# Server settings
 SERVER_PORT = 2332
 SERVER_KEY = b'key'
 
@@ -13,10 +14,15 @@ class CalculationManager(BaseManager):
 
 tasks_queue = Queue()
 results_queue = Queue()
+vector = []
 CalculationManager.register(
     'get_tasks_queue', callable=lambda: tasks_queue)
 CalculationManager.register(
     'get_results_queue', callable=lambda: results_queue)
+
+# Shared memory to distribute vector in order not to duplicate data
+CalculationManager.register(
+    'get_vector', callable=lambda: vector)
 
 manager = CalculationManager(address=('', SERVER_PORT), authkey=SERVER_KEY)
 server = manager.get_server()
